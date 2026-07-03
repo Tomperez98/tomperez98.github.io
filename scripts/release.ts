ab94b728112b67b219b4f0ddb0d594dade26bcb3
Tomperez98 <tomasperezalvarez@gmail.com>
2026-07-02 20:17:59 -0500

    release script

---
+++ scripts/release.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from "node:fs";
+
+const pkg = JSON.parse(readFileSync("package.json", "utf-8"));
+const { version } = pkg;
+const repo = "Tomperez98/formalizr";
+
+const url = new URL(`https://github.com/${repo}/releases/new`);
+url.searchParams.set("title", `v${version}`);
+url.searchParams.set("tag", `v${version}`);
+
+console.log(`Release v${version}:`);
+console.log(url.toString());
